037c6279ede968e166fe599a507515214a99d543
ashain siriwardane <sinfulexiled@gmail.com>
2025-09-04 17:54:50 +0530

    Updates

--- _next/static/chunks/b6ddf3283ed59bd7.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/e56289be741c3c08.js",
-  "static/chunks/turbopack-255f64f6e115255a.js"
-])
--- _next/static/chunks/c70993cea6080a3f.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/e56289be741c3c08.js",
-  "static/chunks/turbopack-c56912640cc3dbf2.js"
-])
