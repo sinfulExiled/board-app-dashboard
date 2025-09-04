7a8177d07baed513e658b04465f3c013419c1952
ashain siriwardane <sinfulexiled@gmail.com>
2025-09-04 15:16:47 +0530

    fix: remove assetprefixg

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/board-app-dashboard",
-  assetPrefix: "/board-app-dashboard",
+
   output: "export",
+  trailingSlash: true,
+
+  images: {
+    unoptimized: true,
+  },
+  devIndicators: false,
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
